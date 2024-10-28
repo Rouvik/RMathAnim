6c3fe247f5a94d8d84d257aabf4c1555c84d853d
Rouvik Maji <majirouvik@gmail.com>
2024-10-28 23:16:02 +0530

    Updated plotAnim.js -> RHist to display a sufficiently good plot, needs numbers yet

--- main.js
+++ main.js
@@ -6,42 +6,21 @@ RGlobal.setContext(cxt);
 
 cxt.lineWidth = 2;
 
-const title = new RAText("Welcome to RMathAnim!", 30, 200, undefined, {maxLineDash: 300, fontSize: 40, backColorFinal: new vec3(48, 124, 248), lineColor: 'rgb(48, 124, 248)'});
+const title = new RAText("Welcome to RMathAnim!", 30, 40, undefined, {maxLineDash: 300, fontSize: 40, backColorFinal: new vec3(48, 124, 248), lineColor: 'rgb(48, 124, 248)'});
 title.ti = 0.005;
 // title.interpolerationFn = (x) => (1 - Math.cos(Math.PI * x)) / 2; // cubic ease-in-out
 title.interpolerationFn = (x) => x < 0.5 ? 4 * x ** 3 : 1 - ((-2 * x + 2) ** 3) / 2; // quadratic ease-in-out
 
-const animVec = new RAVector(new vec2(40, 250), new vec2(550, 250));
-
-const hueVec = new RAHueVector(new vec2(40, 300), new vec2(550, 300));
-hueVec.interpolerationFn = (x) => x < 0.5 ? 4 * x ** 3 : 1 - ((-2 * x + 2) ** 3) / 2; // quadratic ease-in-out
-
-const vec = new RVector(new vec2(20, 300), new vec2(20, 5));
-
-let arr = [];
-const samples = 50;
-for (let i = 0; i < samples; i++) {
-    let mag = ~~(Math.random() * 50 + 100);
-    arr.push(~~(Math.cos(2 * Math.PI / samples * i) * mag), ~~(Math.sin(2 * Math.PI / samples * i) * mag));
-}
-
-// const poly = new RAPoly([50, 50, 100, 50, 100, 100, 50, 100], {maxLineDash: 200});
-const poly = new RAPoly(arr, new vec2(150, 200));
-
-const staticPoly = new RPoly(arr, new vec2(450, 200));
-
 // let told = 0;
+
+const hist = new RHist([9, 6, -3, 7, 5, 2], new vec2(0, 6), new vec2(50, 80), new vec2(500, 300), {strokeColor: 'rgb(255, 255, 255)', mode: RHist.DRAWMODE.stroke | RHist.DRAWMODE.fill});
 
 function animate(t) {
     cxt.clearRect(0, 0, sc.width, sc.height);
-    poly.update();
-    staticPoly.update();
-
-    animVec.update();
     title.update();
-    vec.update();
-    hueVec.update();
-    
+
+    hist.update();
+
     // console.log(1000 / (t - told));
     // told = t;
     
---
+++ src/RMathAnim/2D/plotAnim.js
@@ -0,0 +1,99 @@
+class RHist {
+    static DRAWMODE = {
+        stroke: 1,
+        fill: 2
+    };
+
+    constructor(values, xRange, translate, dimentions, options = {}) {
+        this.values = values;
+        this.translate = translate;
+        this.dimentions = dimentions;
+        this.xRange = xRange;
+
+        this.options = Object.assign({
+            strokeColor: 'rgb(205, 92, 92)',
+            fillColor: 'rgb(135, 206, 235)',
+            segmentGap: 0,
+            heightGap: 0,
+            border: false,
+            displayGraphGuide: true,
+            graphGuideColor: 'rgba(255, 255, 255, 0.2)',
+            graphDeltaGap: 10,
+            graphYDivisions: 10,
+            mode: RHist.DRAWMODE.fill
+        }, options);
+    }
+
+    set values(v) {
+        this._values = v;
+        this.maxVal = this.values[0];
+        this.hasNegValues = false;
+        for (const val of this._values) {
+            if (val < 0) {
+                this.hasNegValues = true;
+            }
+
+            const absVal = Math.abs(val);
+            if (absVal > this.maxVal) {
+                this.maxVal = absVal;
+            }
+        }
+    }
+
+    get values() {
+        return this._values;
+    }
+
+    update() {
+        RGlobal.cxt.beginPath();
+        RGlobal.cxt.strokeStyle = this.options.strokeColor;
+        RGlobal.cxt.fillStyle = this.options.fillColor;
+        if (this.options.border) {
+            RGlobal.cxt.strokeRect(this.translate.x, this.translate.y, this.dimentions.x + 3, this.dimentions.y);
+        }
+
+        let segmentWidth = this.dimentions.x / this.values.length;
+        let baseY = this.hasNegValues ? this.dimentions.y / 2 : this.dimentions.y;
+
+        // draw background graph
+        RGlobal.cxt.moveTo(this.translate.x, this.translate.y - this.options.graphDeltaGap);
+        RGlobal.cxt.lineTo(this.translate.x, this.translate.y + this.dimentions.y + this.options.graphDeltaGap);
+
+        let yDivHeight = this.dimentions.y / this.options.graphYDivisions;
+
+        for (let i = 0; i <= this.options.graphYDivisions; i++) {
+            RGlobal.cxt.moveTo(this.translate.x - this.options.graphDeltaGap, this.translate.y + i * yDivHeight);
+            RGlobal.cxt.lineTo(this.translate.x + this.options.graphDeltaGap, this.translate.y + i * yDivHeight);
+        }
+
+        for (let i = 0; i <= this.values.length; i++) {
+            RGlobal.cxt.moveTo(this.translate.x + i * segmentWidth, this.translate.y + baseY - this.options.graphDeltaGap);
+            RGlobal.cxt.lineTo(this.translate.x + i * segmentWidth, this.translate.y + baseY + this.options.graphDeltaGap);
+        }
+
+        RGlobal.cxt.stroke();
+
+        if (this.options.displayGraphGuide) { // display horizontal graph guide
+            RGlobal.cxt.strokeStyle = this.options.graphGuideColor;
+            for (let i = 0; i <= this.options.graphYDivisions; i++) {
+                RGlobal.cxt.moveTo(this.translate.x + this.options.graphDeltaGap, this.translate.y + i * yDivHeight);
+                RGlobal.cxt.lineTo(this.translate.x + this.dimentions.x, this.translate.y + i * yDivHeight);
+            }
+            RGlobal.cxt.stroke();
+        }
+
+        // draw the histogram here
+        RGlobal.cxt.strokeStyle = this.options.strokeColor;
+        for (let i = 0; i < this.values.length; i++) {
+            let colHeight = this.values[i] / this.maxVal * baseY - this.options.heightGap;
+
+            if (this.options.mode & RHist.DRAWMODE.fill) {
+                RGlobal.cxt.fillRect(this.translate.x + i * segmentWidth + this.options.segmentGap / 2 + 1, this.translate.y + baseY - colHeight, segmentWidth - this.options.segmentGap + 1, colHeight);
+            }
+
+            if (this.options.mode & RHist.DRAWMODE.stroke) {
+                RGlobal.cxt.strokeRect(this.translate.x + i * segmentWidth + this.options.segmentGap / 2 + 1, this.translate.y + baseY - colHeight, segmentWidth - this.options.segmentGap + 1, colHeight);
+            }
+        }
+    }
+}
