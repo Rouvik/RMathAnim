254422ccb4933b67f401cc1378ca1da46f472147
Rouvik Maji <majirouvik@gmail.com>
2025-05-20 01:47:52 +0530

    Added support for inbuilt interpolations in RAnimationInterpolation class and better Compositions support with builder pattern in RComposition class

--- RMathAnim/baseGraphics.js
+++ RMathAnim/baseGraphics.js
@@ -78,6 +78,184 @@ class RAnimation {
     }
 }
 
+class RAnimationInterpolation {
+    static linear(x) {
+        return x;
+    }
+
+    static easeInSine(x) {
+        return 1 - Math.cos((x * Math.PI) / 2);
+    }
+
+    static easeOutSine(x) {
+        return Math.sin((x * Math.PI) / 2);
+    }
+
+    static easeInOutSine(x) {
+        return -(Math.cos(Math.PI * x) - 1) / 2;
+    }
+
+    static easeInQuad(x) {
+        return x * x;
+    }
+
+    static easeOutQuad(x) {
+        return 1 - (1 - x) * (1 - x);
+    }
+
+    static easeInOutQuad(x) {
+        return x < 0.5 ? 2 * x * x : 1 - ((-2 * x + 2) ** 2) / 2;
+    }
+
+    static easeInCubic(x) {
+        return x * x * x;
+    }
+
+    static easeOutCubic(x) {
+        return 1 - ((1 - x) ** 3);
+    }
+
+    static easeInOutCubic(x) {
+        return x < 0.5 ? 4 * x * x * x : 1 - ((-2 * x + 2) ** 3) / 2;
+    }
+
+    static easeInQuart(x) {
+        return x * x * x * x;
+    }
+
+    static easeOutQuart(x) {
+        return 1 - ((1 - x) ** 4);
+    }
+
+    static easeInOutQuad(x) {
+        return x < 0.5 ? 2 * x * x : 1 - ((-2 * x + 2) ** 2) / 2;
+    }
+
+    static easeInQuint(x) {
+        return x * x * x * x * x;
+    }
+
+    static easeOutQuint(x) {
+        return 1 - ((1 - x) ** 5);
+    }
+
+    static easeInOutQuart(x) {
+        return x < 0.5 ? 8 * x * x * x * x : 1 - ((-2 * x + 2) ** 4) / 2;
+    }
+
+    static easeInCirc(x) {
+        x = Math.round(x * 100) / 100;  // avoid precision issues (x cannot be > 1.0)
+        return 1 - (1 - x * x) ** 0.5;
+    }
+    
+    static easeOutCirc(x) {
+        return (1 - ((x - 1) ** 2)) ** 0.5;
+    }
+
+    static easeInOutCirc(x) {
+        return x < 0.5
+            ? (1 - (1 - ((2 * x) ** 2)) ** 0.5) / 2
+            : ((1 - ((-2 * x + 2) ** 2)) ** 0.5 + 1) / 2;
+    }
+
+    static easeInExpo(x) {
+        return x === 0 ? 0 : (2 ** (10 * x - 10));
+    }
+
+    static easeOutExpo(x) {
+        return x === 1 ? 1 : 1 - (2 ** (-10 * x));
+    }
+
+    static easeInOutExpo(x) {
+        return x === 0
+            ? 0
+            : x === 1
+                ? 1
+                : x < 0.5 ? (2 ** (20 * x - 10)) / 2
+                    : (2 - (2 ** (-20 * x + 10))) / 2;
+    }
+
+    static easeInBack(x) {
+        const c1 = 1.70158;
+        const c3 = c1 + 1;
+
+        return c3 * x * x * x - c1 * x * x;
+    }
+
+    static easeOutBack(x) {
+        const c1 = 1.70158;
+        const c3 = c1 + 1;
+
+        return 1 + c3 * ((x - 1) ** 3) + c1 * ((x - 1) ** 2);
+    }
+
+    static easeInOutBack(x) {
+        const c1 = 1.70158;
+        const c2 = c1 * 1.525;
+
+        return x < 0.5
+            ? (((2 * x) ** 2) * ((c2 + 1) * 2 * x - c2)) / 2
+            : (((2 * x - 2) ** 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+    }
+
+    static easeInElastic(x) {
+        const c4 = (2 * Math.PI) / 3;
+
+        return x === 0
+            ? 0
+            : x === 1
+                ? 1
+                : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
+    }
+
+    static easeOutElastic(x) {
+        const c4 = (2 * Math.PI) / 3;
+
+        return x === 0
+            ? 0
+            : x === 1
+                ? 1
+                : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+    }
+
+    static easeInOutElastic(x) {
+        const c5 = (2 * Math.PI) / 4.5;
+
+        return x === 0
+            ? 0
+            : x === 1
+                ? 1
+                : x < 0.5
+                    ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+                    : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+    }
+
+    static easeInBounce(x) {
+        return 1 - RAnimationInterpoleration.easeOutBounce(1 - x);
+    }
+
+    static easeOutBounce(x) {
+        const n1 = 7.5625;
+        const d1 = 2.75;
+
+        if (x < 1 / d1) {
+            return n1 * x * x;
+        } else if (x < 2 / d1) {
+            return n1 * (x -= 1.5 / d1) * x + 0.75;
+        } else if (x < 2.5 / d1) {
+            return n1 * (x -= 2.25 / d1) * x + 0.9375;
+        } else {
+            return n1 * (x -= 2.625 / d1) * x + 0.984375;
+        }
+    }
+
+    static easeInOutBounce(x) {
+        return x < 0.5
+            ? (1 - RAnimationInterpoleration.easeOutBounce(1 - 2 * x)) / 2
+            : (1 + RAnimationInterpoleration.easeOutBounce(2 * x - 1)) / 2;
+    }
+}
+
 /**
  * Base Animation compositions class
  */
@@ -112,18 +290,44 @@ class RComposition {
         this.stepsDone = 0;
         this.currentAnimation = 0;
 
+        this.#check();
+    }
+
+    static compose()
+    {
+        return new RComposition([]);
+    }
+
+    then(fn, steps)
+    {
+        if(typeof fn !== 'function')
+        {
+            throw new RCompositionError("Composition function must be of JS \'function\' type but found: " + typeof fn);
+        }
+        else if (typeof steps !== 'number') {
+            throw new RCompositionError("Composition steps must be of \'number\' type but found: " + typeof steps);
+        }
+
+        this.animRules.push({func: fn, steps});
+
+        return this;
+    }
+
+    #check()
+    {
         // reset steps according to animation requirements
         for (let i = 0; i < this.animRules.length; i++) {
-            if (!this.animRules[i].steps) {
-                throw new Error("[RComposition Error] Bad animation rule set, missing steps");
+            if (!this.animRules[i].func && typeof this.animRules[i].func !== 'function') {
+                throw new RCompositionError("Bad animation rule set, missing func, or invalid \'func\' type: " + typeof this.animRules[i].func);
             }
-
-            this.animRules[i].skip = this.animRules[i].skip | 0;
+            else if (!this.animRules[i].steps && typeof this.animRules[i].steps !== 'number') {
+                throw new RCompositionError("Bad animation rule set, missing steps, or invalid \'steps\' type: " + typeof this.animRules[i].steps);
+            }
         }
     }
 
     /**
-     * Calls update on the animatables in sqeuencial order
+     * Calls update on the animatables in sequential order
      */
     update() {
         if (this.currentAnimation > (this.animRules.length - 1)) {
@@ -141,5 +345,12 @@ class RComposition {
             this.currentAnimation++;
             this.stepsDone = 0;
         }
+    }
+}
+
+class RCompositionError extends Error {
+    constructor(message)
+    {
+        super(`[RComposition ERROR] ${message}`);
     }
 }
--- main.js
+++ main.js
@@ -9,29 +9,36 @@ cxt.lineWidth = 2;
 const title = new RAText("RMathAnim Plot Examples!", 10, 40, undefined, { maxLineDash: 300, fontSize: 40, backColorFinal: new vec3(48, 124, 248), lineColor: 'rgb(48, 124, 248)' });
 title.ti = 0.005;
 
-// title.interpolerationFn = (x) => (1 - Math.cos(Math.PI * x)) / 2; // cubic ease-in-out
-title.interpolerationFn = (x) => x < 0.5 ? 4 * x ** 3 : 1 - ((-2 * x + 2) ** 3) / 2; // quadratic ease-in-out
+title.interpolerationFn = RAnimationInterpolation.easeInOutQuad;
 
 const animVec = new RAVector(new vec2(200, 200), new vec2(400, 200));
-animVec.interpolerationFn = (x) => x < 0.5 ? 4 * x ** 3 : 1 - ((-2 * x + 2) ** 3) / 2; // quadratic ease-in-out
+animVec.interpolerationFn = RAnimationInterpolation.easeOutQuad;
 
-const animVec2 = new RAVector(new vec2(0, 0), new vec2(0, 100), {color: 'rgb(255, 0, 255)'});
-animVec2.interpolerationFn = (x) => x < 0.5 ? 4 * x ** 3 : 1 - ((-2 * x + 2) ** 3) / 2; // quadratic ease-in-out
+const animVec2 = new RAVector(new vec2(0, 0), new vec2(0, 100), { color: 'rgb(255, 0, 255)' });
+animVec2.interpolerationFn = RAnimationInterpolation.linear;
 
-const animComposition = new RComposition([
-    {
-        func: () => title.update(),
-        steps: title.steps + 100
-    },
-    {
-        func: () => {
-            animVec.update();
-            animVec2.RVector.start = animVec.RVector.tip;
-            animVec2.update();
-        },
-        steps: Math.max(animVec.steps, animVec2.steps)
-    }
-]);
+// const animComposition = new RComposition([
+//     {
+//         func: () => title.update(),
+//         steps: title.steps + 100
+//     },
+//     {
+//         func: () => {
+//             animVec.update();
+//             animVec2.RVector.start = animVec.RVector.tip;
+//             animVec2.update();
+//         },
+//         steps: Math.max(animVec.steps, animVec2.steps)
+//     }
+// ]);
+
+const animComposition = RComposition.compose()
+    .then(() => title.update(), title.steps + 100)
+    .then(() => {
+        animVec.update();
+        animVec2.RVector.start = animVec.RVector.tip;
+        animVec2.update();
+    }, Math.max(animVec.steps, animVec2.steps));
 
 function animate() {
     cxt.clearRect(0, 0, sc.width, sc.height);
